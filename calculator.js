992646098c9a24c95b48b736d64f5cde7e8e643f
rabiulalam1 <rabiul.alam@outlook.com>
2022-01-15 15:12:27 -0500

    Done

--- calculator.js
+++ calculator.js
@@ -1,13 +1,22 @@
 function sum(a, b) {
-  return;
+  if (!a) {
+    a = 0
+  }
+  if (!b) {
+    b = 0
+  }
+  return a + b;
 }
 
-function subtract(a, b) {
-  return;
+function subtract(a = 0, b = 0) {
+  return a - b;
 }
 
 function divide(a, b) {
-  return;
+  if (b === 0) {
+    throw new Error(`We cannot divide a number by 0`);
+  }
+  return a / b;
 }
 
 function multiply(a, b) {
